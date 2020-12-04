b94eef9896b6ffc7ab134bed510fd88e41efea97
Ignacio Herráez <ignacio_hg_87@hotmail.com>
2020-12-04 09:34:29 +0100

    I tried to do it different. It worked!

--- hex.js
+++ hex.js
@@ -1,25 +1,82 @@
 
 //=================== VARIABLES ==========================
 
+const hex =[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
 const btn = document.querySelector(".btn");
 const main = document.querySelector("main");
 const span = document.querySelector("h2").firstElementChild;
 const colors =["#ff0000","#b3ff00","#ff00ea","#00b7ff"];
 
 
-//=================== EVENT LISTENERS ==========================
 
 btn.addEventListener("click", ()=>{
-    const randomNumber = getRandomNumber();
-    //console.log(randomNumber);
-    main.style.backgroundColor = colors[randomNumber];
-    span.textContent = `${colors[randomNumber]}`;
+
+    let color = "#";
+    for(let i =0;i<6;i++){
+     color += hex[random6digitos()];
+
+    }
+
+    main.style.backgroundColor =`${color}`;
+    span.textContent = `${color}`
+  
+    
 })
 
-//=================== FUNCTIONS ==========================
+// //=================== FUNCTIONS ==========================
 
-function getRandomNumber(){
+function random6digitos(){
 
-    return Math.round(Math.random()*(colors.length -1));
-
+    return Math.floor(Math.random()* hex.length);
 }
+
+
+
+
+
+// ============================== OTRA FORMA DE HACERLO ==============
+
+
+// //=================== EVENT LISTENERS ==========================
+
+// btn.addEventListener("click", ()=>{
+
+//     let newArray = randomNumber6digitos();
+//     main.style.backgroundColor = `${newArray}`;
+//     span.textContent = `${newArray}`;
+    
+// })
+
+// //=================== FUNCTIONS ==========================
+
+
+// function getRandomNumber(){
+
+//     return Math.round(Math.random()*(colors.length -1));
+
+// }
+
+
+// function randomNumber6digitos (){
+
+// let arrayVacio =["#"];
+// let newArray =[];
+
+// while(arrayVacio.length <=6){
+    
+//     let numeroAleatorio = Math.floor(Math.random() * hex.length);
+
+//     arrayVacio.push(hex[numeroAleatorio]);
+// }
+
+// newArray = arrayVacio.join("");
+
+// return newArray;
+
+
+
+// }
+
+
+
